61e3951d3faa70f88eeb96681dd1c8e4fb3a7c4a
rahulbakkatatla <rahulbhaktala@gmail.com>
2026-05-04 00:49:53 +0530

    fix: correct Firebase projectId and authDomain to worbid

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBS-7DPn8almqe2vjrbclQ1_jO-hN5T2fY",
-  authDomain: "Yeskro.firebaseapp.com",
-  projectId: "Yeskro",
-  storageBucket: "Yeskro.firebasestorage.app",
+  apiKey: "AIzaSyBS-7DPn8almqe2vjrbclQ1_jO-hN5T2fY", 
+  authDomain: "://firebaseapp.com", // This must stay 'worbid'
+  projectId: "worbid",                 // This must stay 'worbid'
+  storageBucket: "worbid.firebasestorage.app",
   messagingSenderId: "819865730242",
   appId: "1:819865730242:web:e3193445bc5db0f83e4f5b"
 }
+
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
